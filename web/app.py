cb4b2da86bb788e5f0d6c92109d28703f41093cb
Jesus L <jeslev4@gmail.com>
2019-03-08 17:49:25 +0100

    first draft interface

---
+++ web/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    #return 'Hello World!'
+    return render_template('dashboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #app.run()
+
+
